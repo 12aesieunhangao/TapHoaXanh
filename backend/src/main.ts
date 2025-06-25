e8ab7a62217e8b6c37ae716a0ceb3b9594f8da59
12aesieunhangao <tran0343517204@gmail.com>
2025-06-25 22:14:02 +0700

    Updatebackend
    Update backend, xóa tap_hoa_xanh.sql

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,13 +5,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-<<<<<<< HEAD
     origin: 'http://localhost:3000',
     credentials: true,
-=======
-    origin: '*',
->>>>>>> pr/10
-  });
+  })
+
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
@@ -20,9 +17,7 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 5000);
-<<<<<<< HEAD
   console.log(`Ứng dụng đang chạy với port: ${process.env.PORT ?? 5000}`);
-=======
->>>>>>> pr/10
+
 }
 bootstrap();
